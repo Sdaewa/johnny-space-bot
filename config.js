5cff9c0990a4e05eebe512e0efeda84c0d240934
Luis Ramirez <sdaewa22@gmail.com>
2021-04-11 19:17:19 +0200

    cleaning up mess

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+}
