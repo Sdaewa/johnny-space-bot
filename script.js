5b9dfd5db45879ca30de3f2ff4bb6c122f2c1783
Luis Ramirez <sdaewa22@gmail.com>
2021-04-11 19:05:04 +0200

    looping thru response

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ const Twitter = require('twitter');
 const config = require('./config.js');
 const Twtr = new Twitter(config);
 
-
+//set search params
 let params = {
     q: '#spaceX',
     count: 10,
     result_type: 'recent',
     lang: 'en'
 }
+
+Twtr.get('search/tweets', params, function (err, data, response) {
+    if (!err) {
+        // Loop through tweets
+        for (let i = 0; i < data.statuses.length; i++) {
+            // Get tweet ID
+            let id = {
+                id: data.statuses[i].id_str
+            }
+            // favorite the tweet
+            T.post('favorites/create', id, function (err, response) {
+                if (err) {
+                    console.log(err[0].message);
+                } else {
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            });
+        }
+    } else {
+        console.log(err);
+    }
+});
