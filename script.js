f1d4da7541be929a99d01e5a66f09e1e3c4cdfdb
Luis Ramirez <sdaewa22@gmail.com>
2021-04-11 18:57:52 +0200

    search params

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const Twitter = require('twitter');
 const config = require('./config.js');
+const Twtr = new Twitter(config);
+
+
+let params = {
+    q: '#spaceX',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
