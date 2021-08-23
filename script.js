10445701bfab2101c0a27221327ed38b0e2ed15c
Luis Ramirez <sdaewa22@gmail.com>
2021-08-23 17:04:31 +0200

    time changed/heroku update

--- script.js
+++ script.js
@@ -1,77 +1,72 @@
-const config = require('./config.js');
-const twit = require('twit');
+const config = require("./config.js");
+const twit = require("twit");
 const T = new twit(config);
 
-
 function reTweet(searchText) {
-    // set search params
-    let params = {
-        q: searchText + '',
-        result_type: 'recent',
-        lang: 'en',
-        count: 10,
-    };
+  // set search params
+  let params = {
+    q: searchText + "",
+    result_type: "recent",
+    lang: "en",
+    count: 10,
+  };
 
-    T.get('search/tweets', params, function (err_search, data_search, response_search) {
+  T.get(
+    "search/tweets",
+    params,
+    function (err_search, data_search, response_search) {
+      let tweets = data_search.statuses;
 
-        let tweets = data_search.statuses
+      if (!err_search) {
+        let tweetIDList = [];
 
-        if (!err_search) {
-
-            let tweetIDList = []
-
-            for (let tweet of tweets) {
-
-                //avoid duplicates
-                if (tweet.text.startsWith('RT @')) {
-                    if (tweet.retweeted_status) {
-
-                        tweetIDList.push(tweet.retweeted_status.id_str);
-                    } else {
-
-                        tweetIDList.push(tweet.id_str);
-                    }
-                } else {
-
-                    tweetIDList.push(tweet.id_str);
-                }
-            };
-
-
-            // unique elements from an array
-            function onlyUnique(value, index, self) {
-                return self.indexOf(value) === index;
+        for (let tweet of tweets) {
+          //avoid duplicates
+          if (tweet.text.startsWith("RT @")) {
+            if (tweet.retweeted_status) {
+              tweetIDList.push(tweet.retweeted_status.id_str);
+            } else {
+              tweetIDList.push(tweet.id_str);
             }
-
-            // unique entries
-            tweetIDList = tweetIDList.filter(onlyUnique)
-
-
-            // retweeting EACH of the tweetID
-            for (let tweetID of tweetIDList) {
-                T.post('statuses/retweet/:id', {
-
-                    id: tweetID
-                }, function (err_rt, data_rt, response_rt) {
-                    if (!err_rt) {
-
-                        console.log("\n\nRetweeted! ID - " + tweetID)
-                    } else {
-
-                        console.log("Duplication found ID => " + tweetID)
-                        console.log("Error = " + err_rt)
-                    }
-                })
-            }
-        } else {
-
-            console.log("Error searching" + err_search)
-            process.exit(1)
+          } else {
+            tweetIDList.push(tweet.id_str);
+          }
         }
-    })
+
+        // unique elements from an array
+        function onlyUnique(value, index, self) {
+          return self.indexOf(value) === index;
+        }
+
+        // unique entries
+        tweetIDList = tweetIDList.filter(onlyUnique);
+
+        // retweeting EACH of the tweetID
+        for (let tweetID of tweetIDList) {
+          T.post(
+            "statuses/retweet/:id",
+            {
+              id: tweetID,
+            },
+            function (err_rt, data_rt, response_rt) {
+              if (!err_rt) {
+                console.log("\n\nRetweeted! ID - " + tweetID);
+              } else {
+                console.log("Duplication found ID => " + tweetID);
+                console.log("Error = " + err_rt);
+              }
+            }
+          );
+        }
+      } else {
+        console.log("Error searching" + err_search);
+        process.exit(1);
+      }
+    }
+  );
 }
 
-// Run every 1 minute
+// Run every 1 hour
 setInterval(function () {
-    reTweet('#spaceX OR #NASA');
-}, 60000)
+  reTweet("#spaceX OR #NASA");
+}, 3600000);
