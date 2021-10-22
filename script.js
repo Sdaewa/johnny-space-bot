9fcb78530c60410a091f92ad2aa6d075a4fa819a
Luis Ramirez <sdaewa22@gmail.com>
2021-10-22 19:09:42 +0200

    right interval time

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const config = require("./config.js");
+const twit = require("twit");
+
 const tweetApod = require("./tweetApod");
 const reTweet = require("./reTweet");
 const tweetNews = require("./tweetNews");
 const tweetReport = require("./tweetReport");
 
+const T = new twit(config);
+
 // RETWEET
-reTweet(
-  "#spaceX OR #Mars OR #Nasa OR #blueOrigin OR #spaceExploration #blackHole"
-);
 setInterval(() => {
   reTweet(
     "#spaceX OR #Mars OR #Nasa OR #blueOrigin OR #spaceExploration #blackHole"
@@ -20,4 +22,4 @@ setInterval(tweetNews, 28800000); //every 8 hour
 setInterval(tweetReport, 28800000); //every 8 hour
 
 // ASTRONOMY IMAGE OF THE DAY
-setInterval(tweetApod, 86, 400, 000); //every 24 hour
+setInterval(tweetApod, 86400000); //every 24 hour
