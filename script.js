41fe3af730f34e4a770aaa2a6e8a86e3bfd0aa04
Luis Ramirez <sdaewa22@gmail.com>
2021-10-26 13:53:13 +0200

    to heroku

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const tweetReport = require("./tweetReport");
 // setInterval(() => {
 //   reTweet(); //every 5 hours
 // }, 18000000);
-// reTweet();
+reTweet();
 
 // NEWS
 
 // setInterval(tweetNews, 28800000); //every 8 hour
-// tweetNews();
+tweetNews();
 
 // ISS DAILY REPORT
 
 // setInterval(tweetReport, 86400000); //every 24 hour
-// tweetReport();
+tweetReport();
 
 // ASTRONOMY IMAGE OF THE DAY
 
