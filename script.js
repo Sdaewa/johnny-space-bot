5b8eef28084c8eaac71701e0d7a629e34eed162a
Luis Ramirez <sdaewa22@gmail.com>
2021-10-26 14:14:29 +0200

    back to intervals

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ const tweetReport = require("./tweetReport");
 
 // RETWEET
 
-// setInterval(() => {
-//   reTweet(); //every 5 hours
-// }, 18000000);
-reTweet();
+setInterval(() => {
+  reTweet(); //every 5 hours
+}, 18000000);
 
 // NEWS
 
-// setInterval(tweetNews, 28800000); //every 8 hour
-tweetNews();
+setInterval(tweetNews, 28800000); //every 8 hour
 
 // ISS DAILY REPORT
 
-// setInterval(tweetReport, 86400000); //every 24 hour
-tweetReport();
+setInterval(tweetReport, 86400000); //every 24 hour
 
 // ASTRONOMY IMAGE OF THE DAY
 
-// setInterval(tweetApod, 86400000); //every 24 hour
-tweetApod();
+setInterval(tweetApod, 86400000); //every 24 hour
